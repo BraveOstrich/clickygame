762a0cbeaa1d5a1bd05885b18de18fdce10ef9a1
Steve White <stevewhitecodes@gmail.com>
2019-04-30 20:21:50 -0500

    Updates

---
+++ precache-manifest.95247911b26261bd1fa803cc448cdf81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6315f1bc090c5de58bebd755556d0e71",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "405145f73248c846ed6d",
+    "url": "/clickygame/static/css/main.d768865d.chunk.css"
+  },
+  {
+    "revision": "00063f64c68494124901",
+    "url": "/clickygame/static/js/2.49afd6f5.chunk.js"
+  },
+  {
+    "revision": "405145f73248c846ed6d",
+    "url": "/clickygame/static/js/main.4b466768.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
